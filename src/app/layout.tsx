858949e0eae3e1917f4d28aa98191c4470c3e5e8
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2025-02-14 03:12:16 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{ background:"#202326" }}
+      <body style={{ background:"#202326",overflow:'clip' }}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
